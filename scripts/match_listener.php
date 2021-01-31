e5b5769a6f0f40618478cb00f6bb3f88c02a8182
Peter Sloots <github@pjeutr.com>
2021-01-31 23:53:33 +0100

    Read keycode and write event in db

--- scripts/match_listener.php
+++ scripts/match_listener.php
@@ -19,16 +19,32 @@ bool canKeyOpenDoor(key, reader) {
 
 */
 
-
-
 require_once '/maasland_app/www/lib/limonade.php';
 require_once '/maasland_app/www/lib/db.php';
 
-$_GET['a']
+//put variables in $_GET
+parse_str(implode('&', array_slice($argv, 1)), $_GET);
+$nr = $_GET['nr'];
+$key = $_GET['key'];
+$reader = $_GET['reader'];
+
+echo "nr=".$nr." key=".$key." reader=".$reader."\n";
 
 $dsn = 'sqlite:/maasland_app/www/db/dev.db';
 try {
         $db = new PDO($dsn);
+        //save event
+        $sql = "INSERT INTO events (nr,keycode,reader,created_at,updated_at) 
+        	VALUES ($nr, $key, $reader, DateTime('now'), DateTime('now'))";
+		$stmt = $db->prepare($sql);
+		$stmt->execute();
+		echo "id=".$db->lastInsertId()."\n";
+		//opendoor
+		if($key == 2581102) {
+			echo "OPEN";
+			
+		}
+
 } catch(PDOException $e) {
         echo "Connexion failed: ".$e;
 }
@@ -38,8 +54,8 @@ option('db_conn', $db);
 
 setlocale(LC_TIME, "nl_NL");
 
-$users = find_object_by_sql("select name from users where id =1");
-echo $users->name;
+// $users = find_object_by_sql("select name from users where id =1");
+// echo $users->name;
 
 
 ?>
